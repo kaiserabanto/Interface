f654227251883bc98a213276126d0dfeb26fdb6e
kaiserabanto <79958102+kaiserabanto@users.noreply.github.com>
2021-05-03 23:05:25 +0800

    Update TestMachine.java

--- kaiser/Machine2/TestMachine.java
+++ kaiser/Machine2/TestMachine.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TestMachine {
 	public static void main(String[] args) {
-		System.out.println("INSERT COIN TO PLAY!");
+		System.out.println("INSERT COIN!");
 		System.out.println("");
 		ArcadeMachine arcadeMachine = new ArcadeMachine();
 		Person person = new Person(5);
